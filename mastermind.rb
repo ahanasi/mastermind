a57ad12829e2085b4b4113a15021ebbd612cffe2
Ahana <ahanasi@seas.upenn.edu>
2020-05-24 21:16:30 -0700

    Combine create_blocks function with colorize_code

--- mastermind.rb
+++ mastermind.rb
@@ -1,23 +1,42 @@
 require_relative "creator.rb"
 require_relative "guesser.rb"
+require 'colorize'
+require 'pry'
+
 class Mastermind
-  attr_reader :game
-  def initialize(computer=Creator.new(),player=Guesser.new())
+  attr_reader :computer
+  attr_reader :player
+  attr_reader :display
+
+  def initialize(computer = Creator.new(), player = Guesser.new(),display = "")
     @computer = computer
     @player = player
+    @display = display
   end
 
-  def create_blocks()
-    red =  " R ".black.on_red
-    green = " G ".black.on_green
-    yellow = " Y ".black.on_yellow
-    blue = " B ".black.on_blue
-    magenta = " M ".black.on_magenta
-    cyan = " C ".black.on_cyan
+  def colorize_code(code)
+    code.each do |item|
+      case item
+      when "R"
+        @display.concat(" R ".black.on_red)
+      when "G"
+        @display.concat(" G ".black.on_green)
+      when "Y"
+        @display.concat(" Y ".black.on_yellow)
+      when "B"
+        @display.concat(" B ".black.on_blue)
+      when "M"
+        @display.concat(" M ".black.on_magenta)
+      when "C"
+        @display.concat(" C ".black.on_cyan)
+      else
+        puts "Didn't expect this :("
+      end
+    end
   end
-
-  def display_code(code)
-
-  end
-  
 end
+
+new_game = Mastermind.new()
+new_game.computer.generate_code
+new_game.colorize_code(new_game.computer.code)
+puts new_game.display
