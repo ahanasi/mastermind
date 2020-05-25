01263b0569223e69e577dde67ad7752f541b5531
Ahana <ahanasi@seas.upenn.edu>
2020-05-24 20:41:45 -0700

    Add function to generate random code

---
+++ creator.rb
@@ -0,0 +1,19 @@
+require_relative "secretcode.rb"
+class Creator
+  attr_reader :code
+  attr_reader :key
+
+  def initialize(code = SecretCode.new(), key = [])
+    @code = code
+    @key = key
+  end
+
+  def generate_code()
+    code_arr = ["R","G","Y","B","M","C"]
+    @code = 4.times.map {code_arr.sample}
+  end
+
+end
+
+test = Creator.new()
+test.generate_code()
