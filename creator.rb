c9cd4fc2aac3f4ab6d6cb73b767487e1b73457d5
Ahana <ahanasi@seas.upenn.edu>
2020-05-26 17:43:27 -0700

    Create function to give feedback at user guess

--- creator.rb
+++ creator.rb
@@ -1,18 +1,36 @@
 require_relative "secretcode.rb"
+require 'colorize'
 
 class Creator
   attr_reader :code
   attr_reader :key
+  attr_reader :feedback
 
-  def initialize(code = SecretCode.new(), key = [])
+  def initialize(code = SecretCode.new(), key = [],feedback = "")
     @code = code
     @key = key
+    @feedback = feedback
   end
 
   def generate_code()
     code_arr = ["R", "G", "Y", "B", "M", "C"]
     @code = 4.times.map { code_arr.sample }
   end
+
+  def give_feedback(guess)
+    @feedback = ""
+    guess.each_with_index do |val,i|
+      if @code[i] == val && @code.include?(val)
+        feedback.concat("\u{26AB}".green)
+      elsif @code.include?(val)
+        feedback.concat("\u{26AB}")
+      end
+    end
+    @feedback
+  end
+
+
+
 end
 
 test = Creator.new()
