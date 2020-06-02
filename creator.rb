fde911b8a5a334d0be07c906316751b7ae714251
Ahana <ahanasi@seas.upenn.edu>
2020-06-02 12:59:19 -0700

    Fix give_feedback function
    If applied, this commit fixes a bug in the feedback function which returned
    an incorrect number of white pegs. The function now takes  into account the total
    number of green pegs before computing the white peg count. It also does not display white
    pegs for duplicate guesses if the corresponding duplicate is not found in the code.

--- creator.rb
+++ creator.rb
@@ -1,8 +1,10 @@
 require_relative "secretcode.rb"
 require "colorize"
+require "set"
+require "pry"
 
 class Creator
-  attr_reader :code
+  attr_accessor :code
   attr_reader :feedback
 
   def initialize(code = SecretCode.new(), feedback = "")
@@ -17,13 +19,29 @@ class Creator
 
   def give_feedback(guess)
     @feedback = ""
-    guess.each_with_index do |val, i|
-      if @code[i] == val && @code.include?(val)
-        feedback.concat("\u{26AB}".green)
-      elsif @code.include?(val)
-        feedback.concat("\u{26AB}")
+    green_count = 0
+    white_count = 0
+    to_delete = []
+    combo_arr = @code.zip(guess)
+    guess_dup = guess.dup
+
+    combo_arr.each do |a,b| 
+      if a==b
+        green_count += 1
+        to_delete.push([a,b])
       end
     end
+
+    to_delete.each_with_index {|val,idx| combo_arr.delete(to_delete[idx])}
+    
+    combo_arr.each do |a,b|
+      if guess_dup.include?(a) 
+        white_count += 1
+      end
+    end 
+
+    green_count.times {feedback.concat("\u{26AB}".green)}
+    white_count.times {feedback.concat("\u{26AB}")}
     puts @feedback
   end
 end
