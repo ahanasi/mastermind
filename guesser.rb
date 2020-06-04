1d11c118cf18b42b7ad3e332436b3367f9572536
Ahana <ahanasi@seas.upenn.edu>
2020-06-04 15:51:54 -0700

    Create computer AI to guess code

--- guesser.rb
+++ guesser.rb
@@ -1,4 +1,6 @@
 require_relative "secretcode.rb"
+require "colorize"
+require "pry"
 
 class Guesser
   attr_reader :guess
@@ -7,4 +9,15 @@ class Guesser
     @guess = guess
   end
 
+  def guess_code(feedback = "")
+    prev_guess = @guess.code
+    @guess.code = SecretCode.new().generate_code
+    if (feedback.split(//).include?("1"))
+      idx = feedback.split(//).index("1")
+      @guess.code[idx] = prev_guess[idx]
+      binding.pry
+    end
+    @guess
+    binding.pry
+  end
 end
